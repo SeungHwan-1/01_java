b61ba9dcbc64045d6541f841902762ff1a02d68f
Lee <cookie3013@naver.com>
2024-08-09 09:06:16 +0900

    chap04

---
+++ chap05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/Question.java
@@ -0,0 +1,15 @@
+package main.java.com.ohgiraffers.section01.array;
+
+public class Question {
+    public static void main(String[] args) {
+        
+        // 트럼프 카드를 랜덤으로 한장 출력해보자
+
+
+        /*
+         *  배열의 길이와 배열의 요소들을 스캐너를 통해 입력받아 정수형 배열을 만들고,
+         *  첫 번째로 큰 값을 출력하는 프로그램을 만들어 주세요. (잘하면 두번째로 큰 값도)
+         *  만약 배열의 길이가 1 이하면 "2보다 짧은 길이의 배열은 두번쨰로 큰 수를 찾을 수 없습니다." 출력
+         * */
+    }
+}
